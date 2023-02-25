efcdbee935bf1f0adb59c46dcf482886163d7ca5
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-02-25 23:15:21 +0200

    Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listIngredients = document.querySelector('#ingredients');
+
+const result = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  return li;
+});
+
+listIngredients.append(...result);
