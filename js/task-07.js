ce3f791c0f0986e1f2ad077c189daef131b6e93f
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-02-26 03:11:45 +0200

    Task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,5 @@
 const inputEl = document.querySelector('#font-size-control');
 const textInput = document.querySelector('#text');
-console.log(textInput.textContent);
 
 const onChangeFontSize = event => {
   textInput.style.fontSize = event.currentTarget.value + 'px';
