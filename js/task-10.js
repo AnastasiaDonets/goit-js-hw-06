903245f963d68d4f9dc75ffc1d1c570a145befb4
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-02-26 08:13:21 +0200

    Task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const container = document.querySelector('#boxes');
+const inputAmount = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+function onGetInputValue() {
+  onCreateBox(inputAmount.value);
+}
+
+const onCreateBox = value => {
+  let arrayBox = [];
+  for (let i = 1; i <= value; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.style.backgroundColor = getRandomHexColor();
+    newDiv.style.height = `${i * 10 + 30}px`;
+    newDiv.style.width = `${i * 10 + 30}px`;
+
+    arrayBox.push(newDiv);
+  }
+  container.append(...arrayBox);
+};
+
+const onDestroyBox = () => {
+  inputAmount.value = 0;
+  container.innerHTML = '';
+};
+
+createBtn.addEventListener('click', onGetInputValue);
+createBtn.addEventListener('click', onCreateBox);
+destroyBtn.addEventListener('click', onDestroyBox);
