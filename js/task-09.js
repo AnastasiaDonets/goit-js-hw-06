d24f3893d67f8f22b3f4af034edae6212ae0991d
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-02-26 03:49:05 +0200

    Task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const span = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const onChangeColor = event => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  span.textContent = newColor;
+};
+
+btn.addEventListener('click', onChangeColor);
