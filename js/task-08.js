bd92d8bfeedaab91231debcf69be4c0c3a39297d
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-02-26 03:34:38 +0200

    Task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const formValues = event.currentTarget.elements;
+
+  const email = formValues.email.value;
+  const password = formValues.password.value;
+
+  const formData = {
+    email,
+    password,
+  };
+
+  if (email === '' || password === '') {
+    alert('Всі поля мають бути заповнені!');
+  } else {
+    console.log(formData);
+  }
+
+  form.reset();
+};
+
+form.addEventListener('submit', onFormSubmit);
